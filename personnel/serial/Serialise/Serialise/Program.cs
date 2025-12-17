2d0743ea1618ab3a0528159eac55ed9f535853c1
MBAMER <163824331+MBAMER@users.noreply.github.com>
2025-12-17 09:27:37 +0100

    Feat(Securhome): Rajout de tous les fichiers pour l'exercices

---
+++ personnel/serial/Serialise/Serialise/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Serialise
+{
+    internal class Program
+    {
+
+    }
+
+    public class Character
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public Actor PlayedBy { get; set; }
+    }
+
+}
